3d9fd4e223ce7e09b306a96d5787751ede8c74ba
rohan-tare <tare.rohan1985@gmail.com>
2024-12-27 15:37:29 +0530

    adding

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require("http")
+const socketio = require("socket.io");
+const path = require("path");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+console.log(path.join(__dirname, "public"))
+io.on("connection", function(socket) {
+    console.log("connected");
+})
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+server.listen(30000);
